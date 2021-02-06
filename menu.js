eb140abf89d9e99607c2b166aeb5ba2af2538fdd
kongames <kon_kostas@windowslive.com>
2021-02-06 22:10:38 +0200

    added help.html plus other things

---
+++ menu.js
@@ -0,0 +1,10 @@
+$(document).ready(()=>{
+    $("#nav").append(`
+    <a class="flex-sm-fill text-sm-center nav-link active" href="index.html">HOME</a>
+        <a class="flex-sm-fill text-sm-center nav-link active" href="videoLectures.html">Video Lectures</a>
+        <a class="flex-sm-fill text-sm-center nav-link active" href="notableProjects.html">Notable Projects</a>
+        <a class="flex-sm-fill text-sm-center nav-link active" href="https://github.com/UomMobileDevelopment">Github</a>
+        <a class="flex-sm-fill text-sm-center nav-link active" href="help.html">Help</a>
+
+    `);
+});
